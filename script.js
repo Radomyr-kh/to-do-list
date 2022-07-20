e1b15c162ba87ab6718a6e34dee6b5092ebaa729
Radomyr Horban <horban.radomyr@gmail.com>
2022-07-20 17:23:34 +0300

    Added LocalStorage

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
+// Local Storage
+let mylocalStorage = JSON.parse(localStorage.getItem("data")) || [];
+console.log(mylocalStorage);
+
+const checkLocalStorage = function () {
+  if (mylocalStorage) {
+    mylocalStorage.forEach(function (x) {
+      $("ol").append(`<li><input type='checkbox' id='${x.id}'>${x.value}</li>`);
+    });
+  }
+};
+checkLocalStorage();
+
+const updateLocalStorage = function (item) {
+  // mylocalStorage = mylocalStorage.filter(function (x) {
+  //   x.value !== item;
+  // });
+  localStorage.setItem("data", JSON.stringify(mylocalStorage));
+};
+
+const addItem = function () {
+  // let dynamicId = 1;
+  const inputBox = $("input:text").val();
+  const timeId = new Date().getTime();
+  if (!inputBox.trim()) {
+    alert("There is nothing to add!");
+  } else {
+    $("ol").append(
+      // `<li><input type='checkbox' id='${inputBox}'>${inputBox}</li>`
+      `<li><input type='checkbox' id='${timeId}'>${inputBox}</li>`
+    );
+    $("input:text").val("");
+
+    // Local Storage
+    mylocalStorage.push({
+      id: `${timeId}`,
+      value: inputBox,
+    });
+    // localStorage.setItem("data", JSON.stringify(mylocalStorage));
+    updateLocalStorage();
+
+    console.log(mylocalStorage);
+    console.log(localStorage.getItem("data"));
+  }
+};
+
 $(document).ready(function () {
-  $("#addItem").click(function () {
-    var inputBox = $("input:text").val();
-    //Input validation (blank string + only whitespaces)
-    if (!inputBox.trim()) {
-      alert("There is nothing to add!");
-    } else {
-      $("ol").append("<li>" + "<input type='checkbox'>" + inputBox + "</li>");
-      $("input:text").val("");
-    }
-  });
+  // Add item on click
+  $("#addItem").click(addItem);
 
   // Add item after pressing Enter key
   $("input:text").keyup(function (event) {
-    if (event.keyCode == 13) {
-      $("#addItem").click();
-      $("input:text").val("");
+    if (event.keyCode === 13) {
+      addItem();
     }
-  });
-
-  // Clear the input
-  $("input").focus(function () {
-    $(this).val("");
   });
 
   // Remove item after the checkbox is clicked
   $(document).on("click", "input:checkbox", function () {
     $(this).parent().addClass("strike").fadeOut("slow");
 
+    let selectedItem = event.target.id;
+    console.log(selectedItem);
+    mylocalStorage = mylocalStorage.filter(function (x) {
+      return x.id !== selectedItem;
+    });
+
+    updateLocalStorage();
+
     // alert when all tasks are completed
     let x = $("ol").children(":visible").length;
-    if (x == 1) {
+
+    if (x === 1) {
       // delay for alert
       setTimeout(function () {
         alert("You have done everything!");
@@ -37,7 +78,6 @@ $(document).ready(function () {
     }
   });
 
-  // jQuery UI
-  // Change the positon of list item with Drag & Drop
+  // Drag & Drop (jQuery UI)
   $("ol").sortable();
 });
